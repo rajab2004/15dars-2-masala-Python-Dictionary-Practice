709be1022bf12410d6c9e2ce49c20e74fdbd99fc
rajab2004 <rajabbekrahmatov2004@gmail.com>
2025-07-11 16:55:35 +0500

    uyga vazifa

--- run.py
+++ run.py
@@ -14,37 +14,37 @@ from randomusers import (
 )
 
 def run_functions() -> None:
-    print("🔹 Full Names:")
+    print(" Full Names:")
     print(get_full_names(randomuser_data))
 
-    print("\n🔹 Users from Netherlands:")
+    print("\n Users from Netherlands:")
     print(get_users_by_country(randomuser_data, "Netherlands"))
 
-    print("\n🔹 Gender Count:")
+    print("\n Gender Count:")
     print(count_users_by_gender(randomuser_data))
 
-    print("\n🔹 Emails of users older than 60:")
+    print("\n Emails of users older than 60:")
     print(get_emails_of_older_than(randomuser_data, 60))
 
-    print("\n🔹 Sorted users by age (descending):")
+    print("\n Sorted users by age (descending):")
     print(sort_users_by_age(randomuser_data, descending=True))
 
-    print("\n🔹 Usernames starting with 'g':")
+    print("\n Usernames starting with 'g':")
     print(get_usernames_starting_with(randomuser_data, "g"))
 
-    print("\n🔹 Average age:")
+    print("\n Average age:")
     print(get_average_age(randomuser_data))
 
-    print("\n🔹 All coordinates:")
+    print("\n All coordinates:")
     print(get_all_coordinates(randomuser_data))
 
-    print("\n🔹 Oldest user:")
+    print("\n Oldest user:")
     print(get_oldest_user(randomuser_data))
 
-    print("\n🔹 Users in timezone +5:30:")
+    print("\n Users in timezone +5:30:")
     print(find_users_in_timezone(randomuser_data, "+5:30"))
 
-    print("\n🔹 Registered before 2010:")
+    print("\n Registered before 2010:")
     print(get_registered_before_year(randomuser_data, 2010))
 
 run_functions()
